807de5bc23bca7b28d9e967d933f47685ae584b0
Simran Sharma <ss1043422@gmail.com>
2025-12-17 11:45:40 +0530

    port

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 require('dotenv').config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const cors = require('cors')
 app.use(express.json());
 app.use(cors());
